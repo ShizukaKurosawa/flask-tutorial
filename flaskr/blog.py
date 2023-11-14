4e4b499daf7a8c6226263160bd476db6fc996ce5
ShizukaKurosawa <gzt.myv0527@gmail.com>
2023-11-14 21:14:17 +0900

    [fix] blog blueprint

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -76,7 +76,7 @@ def update(id):
         error = None
 
         if not title:
-            error = 'Title is not required.'
+            error = 'Title is required.'
 
         if error is not None:
             flash(error)
